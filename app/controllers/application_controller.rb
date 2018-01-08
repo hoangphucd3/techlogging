a777a7b5de35c82c600ce9ca9f998325ef79ccf2
Hoang Phuc Do <phucdh@zigexn.vn>
2018-01-08 17:41:45 +0700

    test jenkins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    render html: 'techlogging'
+    render html: 'techlogging.com'
   end
 end
