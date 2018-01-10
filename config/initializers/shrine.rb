03203b9271a82cf947e7d4c5e2d3759f27b1a0a5
Phuc <phucdh@zigexn.vn>
2018-01-10 22:39:19 +0700

    Add Gemfile

---
+++ config/initializers/shrine.rb
@@ -0,0 +1,14 @@
+require 'shrine'
+require 'shrine/storage/file_system'
+
+Shrine.storages = {
+    cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
+    store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"), # permanent
+}
+
+Shrine.plugin :activerecord
+Shrine.plugin :backgrounding
+Shrine.plugin :logging
+Shrine.plugin :determine_mime_type
+Shrine.plugin :cached_attachment_data
+Shrine.plugin :restore_cached_data
