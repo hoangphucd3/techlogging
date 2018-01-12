b38e404df0638053a29b3bf65ffff6957b4bfe88
Hoang Phuc Do <phucdh@zigexn.vn>
2018-01-12 16:23:21 +0700

    Add devise authenticate for admin page

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,7 @@
 module Admin
   # Admin::BaseController
   class BaseController < ::ApplicationController
+    before_action :authenticate_admin_user!
     layout 'admin/application'
 
     def index; end
