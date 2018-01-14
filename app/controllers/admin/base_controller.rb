b32f64950f88f9a1c1f526304cd814cbb71c36ea
Phuc <phucdh@zigexn.vn>
2018-01-14 14:07:57 +0700

    Enable admin user authenticate

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   # Admin::BaseController
   class BaseController < ::ApplicationController
-    # before_action :authenticate_admin_user!
+    before_action :authenticate_admin_user!
     layout 'admin/application'
 
     def index; end
