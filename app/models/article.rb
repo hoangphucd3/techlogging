2e435c3e8cd3a496bd0f0d5b0d41c0a72c782a2c
Hoang Phuc Do <phucdh@zigexn.vn>
2018-01-15 08:24:49 +0700

    Fix rubocop

--- app/models/article.rb
+++ app/models/article.rb
@@ -16,15 +16,17 @@ class Article < ApplicationRecord
   def build_terms(term_ids)
     term_ids.reject!(&:blank?)
     remove_orphan_terms(term_ids)
-    term_ids.reject(&:blank?).each do |term_id|
-      self.taxonomy_term_relationships.find_or_create_by(taxonomy_term_id: term_id)
+    term_ids.each do |term_id|
+      taxonomy_term_relationships.find_or_create_by(taxonomy_term_id: term_id)
     end
   end
 
   private
 
   def remove_orphan_terms(exclude_term_ids)
-    self.taxonomy_term_relationships.where.not(taxonomy_term_id: exclude_term_ids).destroy_all
+    taxonomy_term_relationships
+      .where.not(taxonomy_term_id: exclude_term_ids)
+      .destroy_all
   end
 
   def should_generate_new_friendly_id?
--- app/models/taxonomy_vocabulary.rb
+++ app/models/taxonomy_vocabulary.rb
@@ -1,6 +1,8 @@
 class TaxonomyVocabulary < ApplicationRecord
-  has_many :taxonomy_terms, inverse_of: :taxonomy_vocabulary, dependent: :destroy
+  has_many :taxonomy_terms,
+           inverse_of: :taxonomy_vocabulary,
+           dependent: :destroy
 
-  scope :topic, -> {  where id: 1 }
-  scope :tag, -> {  where id: 2 }
+  scope :topic, -> { where id: 1 }
+  scope :tag, -> { where id: 2 }
 end
