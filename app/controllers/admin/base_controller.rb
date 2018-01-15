b60ea7a97ee98abdd7058730aad57bc05dde5038
Hoang Phuc <hoangphucd3@gmail.com>
2018-01-15 21:49:34 +0700

    restrict user to admin

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -4,7 +4,7 @@ module Admin
     include Accessible
 
     skip_before_action :check_user, only: :destroy
-    # before_action :authenticate_admin_user!
+    before_action :authenticate_admin_user!
     layout 'admin/application'
 
     def index; end
