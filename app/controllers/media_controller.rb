45d6df7d2551301b8596b1923bc5f5215dc4dd5f
Hoang Phuc Do <phucdh@zigexn.vn>
2018-01-26 14:16:57 +0700

    Updat insert image to editor rendering

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -34,11 +34,15 @@ class MediaController < ApplicationController
   private
 
   def send_to_editor
-    attachment = Photo.find_by(id: params[:attachment][:id])
-    image_size = params[:attachment]['image-size']
+    attachment = Photo.find_by(id: attachment_insert_params[:id])
+                      .editor_render(attachment_insert_params)
+    render_partial = render_to_string('media/_editor_render',
+                                      layout: false,
+                                      formats: [:html],
+                                      locals: { image: attachment })
     render json: {
       success: true,
-      data: attachment.editor_render(image_size)
+      data: render_partial
     }, status: 200
   end
 
@@ -52,5 +56,9 @@ class MediaController < ApplicationController
   def delete_attachment
     attachment = Photo.find_by(id: params[:id])
     attachment.destroy
+  end
+
+  def attachment_insert_params
+    params.require(:attachment)
   end
 end
