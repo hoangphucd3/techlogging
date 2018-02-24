59c675d4bf0e2fff0bcb6b0acb590d3d23f2f306
Phuc <phucdh@zigexn.vn>
2018-02-24 18:04:41 +0700

    Add site name to header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    @articles = Article.all
+    @articles = Article.published
   end
 end
